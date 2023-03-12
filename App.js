82df3c5ce44f226194115773e76efc3003006744
heshanu <heshanu97@gmail.com>
2023-03-12 19:27:04 +0530

    Added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Develop2 brach!</Text>
+      <Text>Develop2 brach!!</Text>
       <StatusBar style="auto" />
     </View>
   );
