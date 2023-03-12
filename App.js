8809a8de6d90c6034887d2241076442f60030c16
heshanu <heshanu97@gmail.com>
2023-03-12 16:16:43 +0530

    change text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Heshan Umayanga</Text>
+      <Text>Heshan Umayanga1</Text>
       <StatusBar style="auto" />
     </View>
   );
