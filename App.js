7b4663fc1c222ead833d4a41dcaa2a1c1aea01d9
heshanu <heshanu97@gmail.com>
2023-03-12 16:26:48 +0530

    changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Heshan Umayanga1233</Text>
+      <Text>Heshan Umayanga12</Text>
       <StatusBar style="auto" />
     </View>
   );
